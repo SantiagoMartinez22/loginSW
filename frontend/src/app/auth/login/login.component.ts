9f02b5455aa70a64c08b2774dc8fb97b38986236
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-05-02 21:00:24 -0500

    Formulario reactivo creado

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public loginForm: FormGroup = this.formBuilder.group({
+    email: ['alejo@gmail.com', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  })
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  public get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  public get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  public login() {
+    if(this.loginForm.valid) {
+      this.router.navigateByUrl('/dashboard');
+      this.loginForm.reset();
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
