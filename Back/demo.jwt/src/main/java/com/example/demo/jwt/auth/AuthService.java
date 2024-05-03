e19ef0ac576750f3a4ae33b7c71c707861a07cc6
SantiagoMartinez22 <smyardev2@gmail.com>
2024-05-03 18:39:03 -0500

    cod completo revisar issue

---
+++ Back/demo.jwt/src/main/java/com/example/demo/jwt/auth/AuthService.java
@@ -0,0 +1,54 @@
+package com.example.demo.jwt.auth;
+import com.example.demo.jwt.jwt.*;
+import com.example.demo.jwt.User.*;
+import com.example.demo.jwt.auth.*;
+
+
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private  final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+
+        //Generar token
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .country(request.getCountry())
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
+    }
+
+}
---
+++ Back/demo.jwt/src/main/java/com/example/demo/jwt/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.jwt.config;
+
+
+import com.example.demo.jwt.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration //Indica que es una clase de configuración
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+
+    //Cadena de filtros que se van a ir trabajando
+    @Bean //Crea el objeto
+    public SecurityFilterChain securityFilterchain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf ->
+                        csrf
+                                .disable())
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
